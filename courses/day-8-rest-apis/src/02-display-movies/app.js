677a4912fa79dd16d2e0a01d169ffe24f5c5a342
timowestland <timo.westland@gmail.com>
2019-07-18 11:57:07 +0200

    Remove solution from excersice 2

--- courses/day-8-rest-apis/src/02-display-movies/app.js
+++ courses/day-8-rest-apis/src/02-display-movies/app.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react'
-import classnames from 'classnames'
 import * as ApiClient from './api-client'
 import './app.css'
 
@@ -11,43 +10,28 @@ function List({ children }) {
   return <ul className="movie-list">{children}</ul>
 }
 
-function Image({ src, alt = '' }) {
-  return <img src={src} alt={alt} />
-}
-
 export function ListItem({ movie }) {
-  const [expanded, setExpanded] = useState(false)
-
   return (
-    <li
-      className={classnames('movie-item', {
-        _is_expanded: expanded,
-      })}
-      onClick={() => setExpanded(!expanded)}
-    >
-      <Image src={`https://image.tmdb.org/t/p/w500/${movie.backdrop_path}`} alt={movie.title} />
+    <li className="movie-item">
       <h3>{movie.title}</h3>
-      <div className="movie-item__content">
-        <p>{movie.overview}</p>
-        <span>
-          Rating: <strong>{movie.vote_average} / 10</strong> ({movie.vote_count} votes)
-        </span>
-      </div>
     </li>
   )
+}
+
+function Image({ src, alt = '' }) {
+  return <img src={src} alt={alt} />
 }
 
 export function App() {
   const [movies, setMovies] = useState([])
 
-  const fetchMovies = async () => {
-    const { results } = await ApiClient.getMovies('popularity.desc')
-    if (results && results.length) {
-      setMovies(results)
-    }
-  }
-
   useEffect(() => {
+    async function fetchMovies() {
+      const { results } = await ApiClient.getMovies('popularity.desc')
+      if (results && results.length) {
+        setMovies(results)
+      }
+    }
     fetchMovies()
   }, [])
 
